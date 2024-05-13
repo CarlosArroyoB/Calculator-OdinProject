a0a7c51b610d55c096d9716bdb7b3a6299465044
CarlosArroyoB <carlos_01_98@hotmail.com>
2024-05-13 14:56:20 -0500

    adding itemsSelect avance

--- itemsSelect.js
+++ itemsSelect.js
@@ -1,13 +1,18 @@
 const botonesNumericos = document.querySelectorAll('.botonNum');
+const botonSum = document.querySelector("#botonMas");
+const display = document.querySelector(".display");
 
-let numero = '';
+let numero1 = '';
+let numero2 = '';
 // Iterar sobre cada botón numérico y agregar un evento click
+
 botonesNumericos.forEach(boton => {
   boton.addEventListener('click', () => {
     let digito = boton.textContent;
-    numero += digito;
-    console.log(numero);
-    console.log(`suma mas 1: ${parseInt(numero)+1}`)
+    numero1 += digito;
+    console.log(numero1);
+    console.log(`suma mas 1: ${parseInt(numero1)+1}`)
+    display.innerHTML = `${numero1}`
   });
 });
 
